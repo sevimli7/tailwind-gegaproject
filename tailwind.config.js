cafdc5c61b2e3f9e8057e880bca382d582c0e9e8
sevimli7 <jevelbrillant@gmail.com>
2023-04-05 15:49:06 +0300

    header kısmı eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+     
+        lg: '1000px',
+        xl: '1000px',
+        '2xl': '1000px',
+      },
+    },
+    extend: {
+      
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+      
+      colors: {
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+
+      spacing: {
+        128: '32rem',
+      },
+    },
   },
   plugins: [],
 }
